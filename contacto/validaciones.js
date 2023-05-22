a0e188cf0871872de3fdb250d9bb732806efabf8
Carolina Soto <caritodeveloper@gmail.com>
2023-05-22 14:35:46 -0300

    intent arreglar form contacto

---
+++ contacto/validaciones.js
@@ -0,0 +1,25 @@
+
+
+function enviarform(event){
+  event.preventDefault();
+  let formulario = document.getElementById('formulario');
+  let nomb = formulario.elements['nombre'].value;
+ 
+  if(nomb.length < 6){
+      alert("El nombre debe tener al menos 6 letras")
+    } else{
+      alert("Datos enviados correctamente");
+      formulario.elements['nombre'].value = "";
+      formulario.elements['apellido'].value = "";
+      formulario.elements['telefono'].value = "";
+      formulario.elements['comentario'].value = "";
+      formulario.elements['mail'].value = "";
+
+    }
+   
+
+}
+
+
+
+
